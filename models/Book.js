537c5d68ba730dfee2babd7bea88ad57440013a7
henkshh <ahmedhassanhenkshh@gmail.com>
2025-11-08 12:03:28 +0200

    your commit message

--- models/Book.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model, Types } = require("mongoose");
-
-const StudentSchema = new Schema({
-  price: { type: Number, required: true },
-  author: { type: Types.ObjectId, ref: "Author", required: true },
-});
-
-module.exports = model("Book", StudentSchema);
